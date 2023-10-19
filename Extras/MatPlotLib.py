75fe5b8b4e2966a0f488b35d19cfd411a40292e3
OksanaNSukhobok <93849417+OksanaNSukhobok@users.noreply.github.com>
2023-10-19 17:26:19 +0500

    Добавление второго тестового графика

--- Extras/MatPlotLib.py
+++ Extras/MatPlotLib.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Пример 1
 # Data for plotting
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2 * np.pi * t)
@@ -13,4 +14,21 @@ ax.set(xlabel='time (s)', ylabel='voltage (mV)',
 ax.grid()
 
 fig.savefig("test.png")
+
+# Пример 2
+plt.style.use('_mpl-gallery')
+
+# make data
+x = np.linspace(0, 10, 100)
+y = 4 + 2 * np.sin(2 * x)
+
+# plot
+fig, ax = plt.subplots()
+
+ax.plot(x, y, linewidth=2.0)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+
 plt.show()
