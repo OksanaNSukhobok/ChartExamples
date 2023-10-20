be089d24256882a589f0ac22e2f330da83ec65b1
OksanaNSukhobok <93849417+OksanaNSukhobok@users.noreply.github.com>
2023-10-20 20:22:27 +0500

    Добавление графиков

--- ChartExamples.py
+++ ChartExamples.py
@@ -2,6 +2,11 @@
 
 import tkinter as tk
 
+# Импорт внешних файлов
+import chart1
+import chart2
+import chart3
+
 # Создание главного окна
 window = tk.Tk()
 window.geometry("450x450")
@@ -10,6 +15,27 @@ window.title("Примеры построения графиков")
 # Добавление метки заголовка
 lblTitle = tk.Label(text = 'Примеры построения графиков', font = ('Helvetica', 16, 'bold'), fg = '#0000cc')
 lblTitle.place(x=55, y=25)
+
+# Добавление кнопки и метки для графика 1
+btnChart1 = tk.Button(window, text = "График 1", font = ('Helvetica', 10, 'bold'), command=chart1.plot_chart)
+btnChart1.place(x=40, y=115, width=90, height=30)
+
+lblChart1 = tk.Label(text = "График синуса matplotlib")
+lblChart1.place(x=170, y=122)
+
+# Добавление кнопки и метки для графика 2
+btnChart2 = tk.Button(window, text = "График 2", font = ('Helvetica', 10, 'bold'), command=chart2.plot_chart)
+btnChart2.place(x=40, y=165, width=90, height=30)
+
+lblChart2 = tk.Label(text = "Нормальное распределение")
+lblChart2.place(x=170, y=172)
+
+# Добавление кнопки и метки для графика 3
+btnChart1 = tk.Button(window, text = "График 3", font = ('Helvetica', 10, 'bold'), command=chart3.plot_chart)
+btnChart1.place(x=40, y=215, width=90, height=30)
+
+lblChart1 = tk.Label(text = "График синуса matplotlib 2")
+lblChart1.place(x=170, y=222)
 
 # Функция закрытия программы
 def do_close():
